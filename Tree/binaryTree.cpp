50b17cc307a78144280a3d94154952bf69cde015
jayparekh4801 <parekhjay4801@gmail.com>
2021-07-28 20:21:50 +0530

    Tree

---
+++ Tree/binaryTree.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std ;
+
+struct Node{
+    int data;
+    struct Node* left, *right;
+
+    Node(int data) {
+        this -> data = data;
+        left = right = NULL;
+    }
+};
+
+void preorder(Node* root) {
+
+    if(root == NULL) {
+        return;
+    }
+
+    cout << root -> data << "\n";
+    preorder(root -> left);
+    preorder(root -> right);
+
+}
+
+void postorder(Node* root) {
+    if(root == NULL) {
+        return;
+    }
+   
+    postorder(root -> left);
+    postorder(root -> right);
+    cout << root -> data << "\n";
+}
+
+void inorder(Node* root) {
+    if(root == NULL) {
+        return;
+    }
+   
+    inorder(root -> left);
+    cout << root -> data << "\n";
+    inorder(root -> right);
+    
+}
+
+int main() {
+
+    struct Node* root = new Node(1);
+    struct Node* first = new Node(2);
+    struct Node* second = new Node(3);
+    struct Node* third = new Node(4);
+    struct Node* fourth = new Node(5);
+    struct Node* fifth = new Node(6);
+    struct Node* sixth = new Node(7);
+
+    root -> left = first;
+    root -> right = second;
+    first -> left = third;
+    first -> right = fourth;
+    second -> left = fifth;
+    second -> right = sixth;
+
+    preorder(root);
+    postorder(root);
+    inorder(root);
+
+
+    return 0;
+}
