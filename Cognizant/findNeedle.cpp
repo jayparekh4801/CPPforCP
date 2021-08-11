3134d9e24732a1886c3c5ee9fe39b05e81f4eae7
jayparekh4801 <parekhjay4801@gmail.com>
2021-08-11 17:10:54 +0530

    findNeedle

---
+++ Cognizant/findNeedle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int findIndex(string para, string needle, int pl, int nl) {
+    int result = -1;
+    for(int i = 0; i < pl - nl + 1; i++) {
+        string temp = para.substr(i, nl);
+        if(temp == needle) {
+            result = i;
+            break;
+        }
+    }
+    return result;
+}
+
+int main() {
+    string para, needle;
+
+    cin >> para;
+    cin >> needle;
+
+    int result = findIndex(para, needle, para.length(), needle.length());
+    cout << result;
+
+    return 0;
+}
+
