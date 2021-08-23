f1355101103ef58b580a1557a03c661053934b81
jayparekh4801 <parekhjay4801@gmail.com>
2021-08-24 05:26:59 +0530

    connectedComponents

---
+++ Graph/connectedComponents.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+void addEdge(vector<int> graph[], int src, int dest) {
+    graph[src].push_back(dest);
+    graph[dest].push_back(src);
+}
+
+void DFS(vector<int> graph[], int v, int src, bool vis[]) {
+    stack<int> s;
+    s.push(src);
+
+    while(!s.empty()) {
+        int temp = s.top();
+        cout << temp;
+        vis[temp] = true;
+        s.pop();
+        for(int i = 0; i < graph[temp].size(); i++) {
+            if(!vis[graph[temp][i]]) {
+                s.push(graph[temp][i]);
+            }
+        }
+    }
+}
+
+void stronglyConnected(vector<int> graph[], int v) {
+    bool vis[v + 1];
+    for(int i = 0; i < v; i++) {
+        vis[i] = false;
+    }
+
+    for(int i = 0; i < v; i++) {
+        if(!vis[i]) {
+            DFS(graph, v, i, vis);
+            cout << "\n";
+            ilands++;
+        } 
+    }
+}
+
+int main() {
+    int v = 5;
+    vector<int> graph[v];
+
+    addEdge(graph, 0, 1);
+    addEdge(graph, 1, 2);
+    addEdge(graph, 3, 4);
+
+    stronglyConnected(graph, v);
+    return 0;
+}
