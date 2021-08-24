5ef6c05f97ca6d5fab06e6c08be89d5e8c2ffbd1
jayparekh4801 <parekhjay4801@gmail.com>
2021-08-24 12:18:40 +0530

    longest common substring

--- Dynamic_Programming/longestCommonSubstring.cpp
+++ Dynamic_Programming/longestCommonSubstring.cpp
@@ -2,24 +2,8 @@
 
 using namespace std;
 
-// Recursive Approach
-
-// int LCS(string s1, string s2, int n, int m) {
-//     if(n == 0 || m == 0) {
-//         return 0;
-//     }
-
-//     if(s1[n - 1] == s2[m - 1]) {
-//         return 1 + LCS(s1, s2, n - 1, m - 1);
-//     }
-//     else {
-//         return max(LCS(s1, s2, n - 1, m), LCS(s1, s2, n, m - 1));
-//     }
-
-// }
-
-// Dynamic Approach
-int LCS(string s1, string s2, int n, int m) {
+int LCSubstring(string s1, string s2, int n, int m) {
+    int result = 0;
     int t[n + 1][m + 1];
     for(int i = 0; i < n + 1; i++) {
         for(int j = 0; j < m + 1; j++) {
@@ -33,14 +17,15 @@ int LCS(string s1, string s2, int n, int m) {
         for(int j = 1; j < m + 1; j++) {
             if(s1[i - 1] == s2[j - 1]) {
                 t[i][j] = 1 + t[i - 1][j - 1];
+                result = max(result, t[i][j]);
             }
             else {
-                t[i][j] = max(t[i][j - 1] , t[i - 1][j]);
+                t[i][j] = 0;
             }
         }
     }
 
-    return t[n][m];
+    return result;
 }
 
 int main() {
@@ -49,7 +34,8 @@ int main() {
     int n = 6;
     int m = 7;
 
-    int result = LCS(s1, s2, n, m);
+    int result = LCSubstring(s1, s2, n, m);
     cout << result;
+
     return 0;
 }
