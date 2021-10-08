f6cb909f20fa6bc8c56c7e8c188e0787ea271fc3
jayparekh4801 <parekhjay4801@gmail.com>
2021-10-08 12:11:49 +0530

    palindrom

---
+++ Strings/isPalindrome.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+static bool isPalindrome(string s) {
+    int l = 0;
+    int r = s.length() - 1;
+    bool result = true;
+
+    while (l < r) {
+        if (s[l] != s[r]) {
+            result = false;
+            break;
+        }
+        l++;
+        r--;
+    }
+
+    return result;
+}
+
+int main() {
+    string b = "aabbaa43";
+    bool result = isPalindrome(b);
+    cout << result;
+    return 0;
+}
