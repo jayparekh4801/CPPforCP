ffcd23ff9783d5827c438f3602b17f6839eb030e
jayparekh4801 <parekhjay4801@gmail.com>
2022-02-22 01:46:03 +0530

    largest subarray with 0 sum

---
+++ Arrays/largestSubarray0sum.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int largestSubarray(int a[], int n) {
+    map<int, int> m;
+    int s = 0;
+    int result = 0;
+
+    for(int i = 0; i < n; i++) {
+        s += a[i];
+
+        if(s == 0) {
+            result = max(result, i + 1);
+        }
+
+        else if(m.find(s) == m.end()) {
+            m.insert(make_pair(s, i));
+        }
+        else {
+            int cl = i - m[s];
+            result = max(cl, result);
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int result = largestSubarray(a, n);
+
+    cout << result;
+
+    return 0;
+}
